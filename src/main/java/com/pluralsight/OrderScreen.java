43a8d11238e79ccd7776a65c9b440191d7738153
AbigayleY <youngabigayle@gmail.com>
2026-05-26 15:04:26 -0700

    Added more classes! made Enums, services, and models. Fixed gitHub Project board and got rid of Step By Step file here.

---
+++ src/main/java/com/pluralsight/OrderScreen.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class OrderScreen {
+    static Scanner scanner = new Scanner(System.in);
+
+     static void main(){
+        System.out.println("\n -----------------------------");
+        System.out.println();
+        System.out.println("1) Add Pizza");
+        System.out.println("2) Add Drink");
+        System.out.println("3)Add Garlic Knots");
+        System.out.println("0) Cancel Order");
+        int choice = scanner.nextInt();
+
+        switch  (choice){
+            case 1:
+
+        }
+
+    }
+    public
+}
